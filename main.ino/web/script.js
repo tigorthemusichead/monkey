9539279dd8d04d81888513cd82b224fe5e7bf819
tigorthemusichead <arsenij.ermolaev2003@gmail.com>
2022-07-18 02:10:44 +0300

    timer logic

--- main.ino/web/script.js
+++ main.ino/web/script.js
@@ -3,6 +3,11 @@ const timerMenuButtonOpen = document.querySelector('.open-timers');
 const timerMenuButtonCLose = document.querySelector('.close-timers');
 const timerWrapper = document.querySelector('.timer-wrapper');
 const timerCheckboxes = document.querySelectorAll('.checkbox');
+const headerInfo = document.querySelector('.header-box');
+const time = document.querySelector('.time');
+const timerForms = document.querySelectorAll('.timer');
+const noTimerButton = document.querySelector('.no-timer');
+const eraseTimersButton = document.querySelector('.erase');
 
 /**
  * UX/UI Code
@@ -27,18 +32,75 @@ timerWrapper.addEventListener('touchend', (e)=>{
     }
 })
 
+/**
+ * Timers logic
+ */
+
 timerCheckboxes.forEach((checkbox, index) => {
     checkbox.addEventListener('click', ()=>{
-        timerCheckboxes.forEach(checkbox => {
-            checkbox.classList.remove('checkbox-active');
-        });
+        timerCheckboxes[timers.activeIndex]?.classList.remove('checkbox-active');
         checkbox.classList.add('checkbox-active');
+        timers.activeIndex = index;
+        setTimerInfo(index);
     })
 });
 
-window.addEventListener("load",function() {
-    setTimeout(function(){
-        // This hides the address bar:
-        window.scrollTo(0, 1);
-    }, 0);
-});
+timerForms.forEach((form, index) => {
+    form.addEventListener('change', ()=>{
+        timers.info[index] = {
+            name: form.name.value,
+            min: form.min.value,
+            sec: form.sec.value
+        }
+        if(index === timers.activeIndex)
+            setTimerInfo(index);
+    })
+})
+
+noTimerButton.addEventListener('click', ()=>{
+    timerCheckboxes[timers.activeIndex]?.classList.remove('checkbox-active');
+    timers.activeIndex = null;
+    setTimerInfo(10);
+})
+
+eraseTimersButton.addEventListener('click', ()=>{
+    timerCheckboxes[timers.activeIndex]?.classList.remove('checkbox-active');
+    timers.activeIndex = null;
+    setTimerInfo(10);
+    timers.info.forEach((timer, index) => {
+        if(index < 10) timer = {name: null, min: null, sec: null};
+    })
+    timerForms.forEach(form => {
+        form.name.value = form.min.value = form.sec.value = null;
+    })
+})
+
+let timers = {
+    activeIndex: null,
+    info: [
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: null, min: null, sec: null},
+        {name: 'Без Таймера', min: null, sec: null}
+    ]
+}
+
+function timeUtil(min, sec){
+    if (!min && !sec)
+        return '--:--';
+    min = `0${min}`;
+    sec = sec < 10 ? `0${sec}` : `${sec}`;
+    return min + ':' + sec;
+}
+
+function setTimerInfo(index){
+    headerInfo.innerHTML = timers.info[index].name || "Безымянный таймер";
+    time.innerHTML = timeUtil(timers.info[index].min, timers.info[index].sec);
+}
