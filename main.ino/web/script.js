fceb7a35b56dfaf48fed7ec652d9a27d41be3d30
tigorthemusichead <arsenij.ermolaev2003@gmail.com>
2022-07-18 00:46:05 +0300

    design ready

--- main.ino/web/script.js
+++ main.ino/web/script.js
@@ -1,4 +1,37 @@
-const kek = document.querySelector('.kek');
-kek.addEventListener('click', ()=>{
-    alert(1);
+const timerMenuButton = document.querySelector('.timer-button-wrapper');
+const timerMenuButtonOpen = document.querySelector('.open-timers');
+const timerMenuButtonCLose = document.querySelector('.close-timers');
+const timerWrapper = document.querySelector('.timer-wrapper');
+const timerCheckboxes = document.querySelectorAll('.checkbox');
+
+/**
+ * UX/UI Code
+ */
+
+let touchStartY = 0;
+
+timerMenuButton.addEventListener('touchstart', (e)=>{
+    touchStartY = e.changedTouches[0].screenY
 })
+
+timerMenuButton.addEventListener('touchend', (e)=>{
+    if(e.changedTouches[0].screenY > touchStartY){
+        timerMenuButtonOpen.style.display = 'inherit';
+        timerMenuButtonCLose.style.display = 'none';
+        timerWrapper.style.top = 'calc(100vh - 170px)';
+    }
+    else{
+        timerMenuButtonOpen.style.display = 'none';
+        timerMenuButtonCLose.style.display = 'inherit';
+        timerWrapper.style.top = '14px';
+    }
+})
+
+timerCheckboxes.forEach((checkbox, index) => {
+    checkbox.addEventListener('click', ()=>{
+        timerCheckboxes.forEach(checkbox => {
+            checkbox.classList.remove('checkbox-active');
+        });
+        checkbox.classList.add('checkbox-active');
+    })
+});
